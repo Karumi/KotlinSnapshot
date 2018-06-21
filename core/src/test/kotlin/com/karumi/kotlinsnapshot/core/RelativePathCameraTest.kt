1221ff035c65eb29c27f810b16ee5fb46d756066
Sergio Gutierrez <sergio@gokarumi.com>
2018-06-21 16:17:25 +0200

    Fix snapshot paths

--- core/src/test/kotlin/com/karumi/kotlinsnapshot/core/RelativePathCameraTest.kt
+++ core/src/test/kotlin/com/karumi/kotlinsnapshot/core/RelativePathCameraTest.kt
@@ -3,7 +3,7 @@ package com.karumi.kotlinsnapshot.core
 import org.junit.Test
 
 class RelativePathCameraTest {
-    private val camera = Camera(relativePath = "src/test/kotlin/com/kotlinsnapshot")
+    private val camera = Camera(relativePath = "src/test/kotlin/com/karumi/kotlinsnapshot/core")
 
     @Test
     fun should_take_snapshots_and_store_them_in_the_provided_relative_path() {
