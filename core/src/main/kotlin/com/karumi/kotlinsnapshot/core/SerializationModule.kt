58a3fdf1469e376c4098ab6c0228a1954a4908a1
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-11-07 09:17:22 +0100

    Fix ktlint issues

--- core/src/main/kotlin/com/karumi/kotlinsnapshot/core/SerializationModule.kt
+++ core/src/main/kotlin/com/karumi/kotlinsnapshot/core/SerializationModule.kt
@@ -3,7 +3,7 @@ package com.karumi.kotlinsnapshot.core
 import com.google.gson.GsonBuilder
 import com.karumi.kotlinsnapshot.core.serializers.RuntimeClassNameTypeAdapterFactory
 import com.karumi.kotlinsnapshot.serializers.GsonUTCDateAdapter
-import java.util.*
+import java.util.Date
 
 interface SerializationModule {
 
@@ -26,5 +26,4 @@ class KotlinSerialization : SerializationModule {
         is String -> value
         else -> gson.toJson(value)
     }
-
 }
